7d7f1252d1079de7ec93da60bc6100380dcb02b4
rhmn5ty <rahman.satya@gmail.com>
2023-04-20 17:26:17 +0700

    update sim

--- src/Sim.java
+++ src/Sim.java
@@ -1,3 +1,108 @@
 public class Sim {
 
+    // Atribut
+    private String nama;
+    private Pekerjaan pekerjaan;
+    private int uang;
+    private Inventory<Item> inventory;
+    private int kekenyangan;
+    private int mood;
+    private int kesehatan;
+    private String status;
+    private Point posisi;
+    private Rumah rumah;
+    private Rumah posisiRumah;
+    private Ruangan posisiRuangan;
+
+    // Konstruktor
+    public Sim() {
+
+    }
+
+    // Method
+    public String getNama() {
+        return nama;
+    }
+
+    public Pekerjaan getPekerjaan() {
+        return pekerjaan;
+    }
+
+    public void setPekerjaan(Pekerjaan pekerjaan) {
+        this.pekerjaan = pekerjaan;
+    }
+
+    public int getUang() {
+        return uang;
+    }
+
+    public void setUang(int uang) {
+        this.uang = uang;
+    }
+
+    public Inventory<Item> getInventory() {
+        return inventory;
+    }
+
+    public int getKekenyangan() {
+        return kekenyangan;
+    }
+
+    public void setKekenyangan(int kekenyangan) {
+        this.kekenyangan = kekenyangan;
+    }
+
+    public int getMood() {
+        return mood;
+    }
+
+    public void setMood(int mood) {
+        this.mood = mood;
+    }
+
+    public int getKesehatan() {
+        return kesehatan;
+    }
+
+    public void setKesehatan(int kesehatan) {
+        this.kesehatan = kesehatan;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Point getPosisi() {
+        return posisi;
+    }
+
+    public void setPosisi(Point posisi) {
+        this.posisi = posisi;
+    }
+
+    public Rumah getRumah() {
+        return rumah;
+    }
+
+    public Rumah getPosisiRumah() {
+        return posisiRumah;
+    }
+
+    public void setPosisiRumah(Rumah posisiRumah) {
+        this.posisiRumah = posisiRumah;
+    }
+
+    public Ruangan getPosisiRuangan() {
+        return posisiRuangan;
+    }
+
+    public void setPosisiRuangan(Ruangan posisiRuangan) {
+        this.posisiRuangan = posisiRuangan;
+    }
+
+    // Aksi
 }
