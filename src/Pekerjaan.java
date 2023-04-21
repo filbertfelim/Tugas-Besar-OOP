1a393683c850738cb077ce47603e4b5c179b6b25
Pramaditya Fajri Migfar <89334139+fajrimigfar@users.noreply.github.com>
2023-04-21 17:20:38 +0700

    Update Pekerjaan.java

--- src/Pekerjaan.java
+++ src/Pekerjaan.java
@@ -1,3 +1,45 @@
-public class Pekerjaan {
+import java.util.concurrent.ThreadLocalRandom;
 
+public class Pekerjaan {
+    private String nama;
+    private int gaji;
+    private String[] listPekerjaan = {"Badut Sulap", "Koki", "Polisi", "Programmer", "Dokter"};
+
+    public Pekerjaan() {
+        nama = listPekerjaan[ThreadLocalRandom.current().nextInt(0, listPekerjaan.length)];
+        if (nama.equals("Badut Sulap")) {
+            gaji = 15;
+        } else if (nama.equals("Koki")) {
+            gaji = 30;
+        } else if (nama.equals("Polisi")) {
+            gaji = 35;
+        } else if (nama.equals("Programmer")) {
+            gaji = 45;
+        } else if (nama.equals("Dokter")) {
+            gaji = 50;
+        }
+    }
+
+    public Pekerjaan(String nama) {
+        this.nama = nama;
+        if (nama.equals("Badut Sulap")) {
+            gaji = 15;
+        } else if (nama.equals("Koki")) {
+            gaji = 30;
+        } else if (nama.equals("Polisi")) {
+            gaji = 35;
+        } else if (nama.equals("Programmer")) {
+            gaji = 45;
+        } else if (nama.equals("Dokter")) {
+            gaji = 50;
+        }
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public int getGaji() {
+        return gaji;
+    }
 }
