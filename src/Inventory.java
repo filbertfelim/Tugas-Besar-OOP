5f5dd0b23d8078d8bca620df44f87f51b48a5637
Pramaditya Fajri Migfar <89334139+fajrimigfar@users.noreply.github.com>
2023-04-21 23:53:14 +0700

    Update Inventory.java

--- src/Inventory.java
+++ src/Inventory.java
@@ -1,3 +1,68 @@
-public class Inventory<Item> {
+import java.util.ArrayList;
 
+public class Inventory {
+    private ArrayList<Item> items;
+    private ArrayList<Integer> jumlah;
+
+    public Inventory() {
+        items = new ArrayList<Item>();
+        jumlah = new ArrayList<Integer>();
+    }
+
+    public ArrayList<Item> getItems() {
+        return items;
+    }
+
+    public ArrayList<Integer> getJumlah() {
+        return jumlah;
+    }
+
+    public void add(Item item) {
+        boolean check = false;
+        int index = -1;
+
+        for (Item i : items) {
+            index++;
+            if (i.getNamaItem() == item.getNamaItem()) {
+                check = true;
+                break;
+            }
+        }
+        if (check) {
+            jumlah.set(index, jumlah.get(index) + 1);
+        } else {
+            items.add(item);
+            jumlah.add(1);
+        }
+    }
+
+    public void remove(Item item) throws Exception {
+        boolean check = false;
+        int index = -1;
+
+        for (Item i : items) {
+            index++;
+            if (i.getNamaItem() == item.getNamaItem()) {
+                check = true;
+                break;
+            }
+        }
+
+        if (check) {
+            jumlah.set(index, jumlah.get(index) - 1);
+            if (jumlah.get(index) == 0) {
+                items.remove(index);
+                jumlah.remove(index);
+            }
+        } else {
+            throw new Exception("Kamu tidak mempunyai objek tersebut!");
+        }
+    }
+
+    public void printInventory() {
+        for (int i = 0; i < items.size(); i++) {
+            System.out.print(items.get(i) + ":   ");
+            System.out.println(jumlah.get(i));
+        }
+    }
 }
