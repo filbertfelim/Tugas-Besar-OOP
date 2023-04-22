73b5221569d2286aaad713d9f0b56be3bd26c998
IbnuGifari <110520097+IbnuGifari@users.noreply.github.com>
2023-04-23 06:18:23 +0700

    update Ruangan dan Rumah

--- src/Ruangan.java
+++ src/Ruangan.java
@@ -1,3 +1,41 @@
-public class Ruangan {
+import java.util.ArrayList;
 
+public class Ruangan {
+    private int panjang;
+    private int lebar;
+    private Point lokasi;
+    private static ArrayList<Item> listofItems;
+
+    public Ruangan(Rumah rumah, Point lokasi)
+    {
+        panjang = 6;
+        lebar = 6;
+        this.lokasi = lokasi;
+        listofItems = new ArrayList<Item>();
+    }
+
+    public Point getLokasiRuangan()
+    {
+        return lokasi;
+    }
+
+    public void setLokasiRuangan(Point lokasi)
+    {
+        this.lokasi = lokasi;
+    }
+
+    public int getPanjang()
+    {
+        return panjang;
+    }
+
+    public int getLebar(Point lokasi)
+    {
+        return lebar;
+    }
+
+    public static ArrayList<Item> getListofItems()
+    {
+        return listofItems;
+    }
 }
