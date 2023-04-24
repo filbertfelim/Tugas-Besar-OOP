2e7e19ea22c521e7abecf53cdcbb381f4a3ee1c4
Bobby Dhammawan Tang <75462550+bobbydt@users.noreply.github.com>
2023-04-24 13:21:32 +0700

    Update Ruangan.java

--- src/Ruangan.java
+++ src/Ruangan.java
@@ -1,41 +1,58 @@
 import java.util.ArrayList;
 
 public class Ruangan {
+    private String namaRuangan;
+    private int ruanganKe;
     private int panjang;
     private int lebar;
-    private Point lokasi;
-    private static ArrayList<Item> listofItems;
+    private int[][] matriksRuangan;
+    private ArrayList<NonMakanan> listOfObjek;
+    
 
-    public Ruangan(Rumah rumah, Point lokasi)
-    {
-        panjang = 6;
-        lebar = 6;
-        this.lokasi = lokasi;
-        listofItems = new ArrayList<Item>();
+    // Konstruktor
+    public Ruangan(String namaRuangan, int ruanganKe, int panjang, int lebar) {
+        this.namaRuangan = namaRuangan;
+        this.ruanganKe = ruanganKe;
+        this.panjang = panjang;
+        this.lebar = lebar;
+        matriksRuangan = new int[panjang][lebar];
+        listOfObjek = new ArrayList<NonMakanan>();
     }
 
-    public Point getLokasiRuangan()
-    {
-        return lokasi;
+    // Getter
+    public String getNamaRuangan() {
+        return namaRuangan;
+    }
+    
+    public int getRuanganKe() {
+        return ruanganKe;
     }
 
-    public void setLokasiRuangan(Point lokasi)
-    {
-        this.lokasi = lokasi;
+    public int getPanjang() {
+        return panjang; 
     }
 
-    public int getPanjang()
-    {
-        return panjang;
-    }
-
-    public int getLebar(Point lokasi)
-    {
+    public int getLebar() {
         return lebar;
     }
 
-    public static ArrayList<Item> getListofItems()
-    {
-        return listofItems;
+    public int[][] getMatriksRuangan() {
+        return matriksRuangan;
+    }
+
+    public ArrayList<NonMakanan> getListOfObjek() {
+        return listOfObjek;
+    }
+
+    public void memasangBarang(Item item, Point point) {
+        
+    }
+
+    public static ArrayList<NonMakanan> getListOfObjek() {
+        return listOfObjek;
+    }
+
+    public void memindahBarang() {
+        
     }
 }
