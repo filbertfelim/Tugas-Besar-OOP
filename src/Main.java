63f8de32a3793f61ef87c61baf320871d3387103
Bobby Dhammawan Tang <75462550+bobbydt@users.noreply.github.com>
2023-04-25 00:50:10 +0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,38 @@
+import java.util.Scanner;
+
 public class Main {
+    
+    public static void main(String []args) {
+        boolean exit = false;
+        System.out.println("Sim-Plicity");
+        System.out.print("1. New World\n2. Load World\n3. Exit");
+        Scanner readInput = new Scanner(System.in);
+        System.out.print("Enter the number: ");
+        String input = readInput.next();
+        switch (input) {
+            case "1":
+                // addSim
+                // addWorld
+                while(!exit) //gameplay
+                {
+                    
+                }
+                break; 
+            case "2":
+                // Check for avaible world,
+                while(!exit) //gameplay
+                {
+                    
+                }
+                break;
+            case "3":
+                System.out.println("Goodbye");
+                break;
+        }
+        
+
+        
+    }
+    
 
 }
