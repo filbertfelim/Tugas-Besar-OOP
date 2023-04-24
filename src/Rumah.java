780546ebc329433ccec31770d431527b37a580f1
Bobby Dhammawan Tang <75462550+bobbydt@users.noreply.github.com>
2023-04-25 00:08:09 +0700

    Update Rumah.java

--- src/Rumah.java
+++ src/Rumah.java
@@ -1,28 +1,21 @@
 import java.util.ArrayList;
 
 public class Rumah {
-    
     private String namaRumah;
     private Point lokasi;
-    private int panjang;
-    private int lebar;
     private ArrayList<Ruangan> listOfRuangan = new ArrayList<Ruangan>();
-    private int[][] matriksRumah;
-
+   
     // Konstruktor
 
     /**
      * @param namaRumah
      * @param lokasi
-     * @param panjang
-     * @param lebar
      */
-    Rumah (String namaRumah, Point lokasi, int panjang, int lebar) {
+    Rumah (String namaRumah, Point lokasi) {
         this.namaRumah = namaRumah;
         this.lokasi = lokasi;
-        this.panjang = panjang;
-        this.lebar = lebar;
-        matriksRumah = new int[panjang][lebar];
+        Ruangan ruang = new Ruangan("kamar");
+        listOfRuangan.add(ruang);
     }
 
     // Getter dan Setter
@@ -39,27 +32,15 @@ public class Rumah {
     public void setLokasi(Point lokasi) {
         this.lokasi = lokasi;
     }
-    public int getPanjang() {
-        return panjang;
-    }
-    public void setPanjang(int panjang) {
-        this.panjang = panjang;
-    }
-    public int getLebar() {
-        return lebar;
-    }
-    public void setLebar(int lebar) {
-        this.lebar = lebar;
-    }
+    
     public ArrayList<Ruangan> getListOfRuangan() {
         return listOfRuangan;
     }
     public void setListOfRuangan(ArrayList<Ruangan> listOfRuangan) {
         this.listOfRuangan = listOfRuangan;
     }
-    public void tambahRuangan()
+    public void tambahRuangan(Ruangan ruang) {
+        listOfRuangan.add(ruang);
+    }
     
-
-    
-
 }
