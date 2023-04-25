decb660fa1382dae7ba17937cceca2f42d781547
Bobby Dhammawan Tang <75462550+bobbydt@users.noreply.github.com>
2023-04-25 15:34:00 +0700

    Update Ruangan.java

--- src/Ruangan.java
+++ src/Ruangan.java
@@ -3,33 +3,26 @@ import java.util.ArrayList;
 public class Ruangan {
     private String namaRuangan;
     private int ruanganKe;
-    private final int panjang = 6;
+    private final int panjang = 6; 
     private final int lebar = 6;
-    private int[][] matriksRuangan;
+    private int[][] matriksRuangan = new int[panjang][lebar];
     private ArrayList<NonMakanan> listOfObjek;
-    
 
     // Konstruktor
     public Ruangan(String namaRuangan, int ruanganKe) {
         this.namaRuangan = namaRuangan;
         this.ruanganKe = ruanganKe;
-        matriksRuangan = [panjang][lebar];
-        listOfObjek = new ArrayList<NonMakanan>();
     }
 
     // Getter
     public String getNamaRuangan() {
         return namaRuangan;
     }
-    
-    public int getRuanganKe() {
-        return ruanganKe;   
+
+    public ArrayList<NonMakanan> getListOfObjek() {
+        return listOfObjek;
     }
-    
-    public int setRuanganKe(int ruanganKe) {
-        this.ruanganKe = ruanganKe;
-    }
-       
+
     public int getPanjang() {
         return panjang; 
     }
@@ -38,20 +31,8 @@ public class Ruangan {
         return lebar;
     }
 
-    public int[][] getMatriksRuangan() {
-        return matriksRuangan;
-    }
-
-    public ArrayList<NonMakanan> getListOfObjek() {
-        return listOfObjek;
-    }
-    
-    public void printListOfObjek() {
-        
-    }
-    
     public void memasangBarang(Item item, Point point) {
-        listOfObjek.add(item);    
+
     }
 
     public void memindahBarang() {
