7f3f47333c2e53408816c7cad0b122c1ad2d59bb
Satya <rahman.satya@gmail.com>
2023-04-25 19:57:32 +0700

    update fitur load

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 import java.util.*;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import java.lang.Math;
+
 public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
@@ -9,5 +20,48 @@ public class Main {
         World.addSim(scan);
         World.changeSim(scan);
         System.out.println(World.getActiveSim().getNama());
+    }
+
+    private static void load(String namaFile) {
+        JSONParser jsonP = new JSONParser();
+
+        try (FileReader reader = new FileReader(namaFile + ".json")) {
+            // Read JSON File
+            Object obj = jsonP.parse(reader);
+            JSONArray objList = (JSONArray) obj;
+            System.out.println(objList);
+            // Iterate over emp array
+            objList.forEach(object -> parseSim((JSONObject) object));
+            objList.forEach(object -> parseWorld((JSONObject) object));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void parseSim(JSONObject object) {
+        try {
+            JSONObject simObj = (JSONObject) object.get("Sim");
+
+            String nama = (String) simObj.get("nama");
+            Pekerjaan pekerjaan;
+            int uang = Math.toIntExact((Long) simObj.get("uang"));
+            Inventory inventory;
+            int kekenyangan = Math.toIntExact((Long) simObj.get("kekenyangan"));
+            int mood = Math.toIntExact((Long) simObj.get("mood"));
+            int kesehatan = Math.toIntExact((Long) simObj.get("kesehatan"));
+            String status = (String) simObj.get("status");
+            Point posisi; // di dalam rumah
+            Rumah rumah;
+            Rumah posisiRumah;
+            Ruangan posisiRuangan;
+
+        } catch (NullPointerException e) {
+
+        }
+
     }
 }
