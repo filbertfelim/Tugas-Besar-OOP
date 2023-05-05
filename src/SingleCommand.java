38fcb5e3c4952bfbf69f2b3bdc956cada70a9345
IbnuGifari <110520097+IbnuGifari@users.noreply.github.com>
2023-05-05 19:45:49 +0700

    Update Help via SingleCommand and SingleHTP

--- src/SingleCommand.java
+++ src/SingleCommand.java
@@ -13,27 +13,29 @@ public class SingleCommand {
     }
  
     public void showMessage(){
-        System.out.println("Help dan Exit akan melakukan hal yang sama seperti yang sudah dijelaskan sebelumnya");
-        System.out.println("View Sim Info akan menampilkan profil dan stats dari Sim yang sedang dimainkan");
-        System.out.println("View Current Location akan menampilkan lokasi persis dari Sim yang sedang dimainkan");
-        System.out.println("View Inventory akan menampilkan barang apa yang dimiliki Sim yang sedang dimainkan ");
-        System.out.println("Upgrade House akan meminta kamu untuk memasukkan ruangan apa yang akan ditambahkan ruangan tetangga,");
-        System.out.println("sehingga dapat dikatakan bahwa menu Upgrade House berarti menambah ruangan dalam rumah");
-        System.out.println("Move Room akan memindahkan ruangan yang saling terhubung agar dapat dpindahkan");
-        System.out.println("Edit Room akan meminta kamu untuk memilih 2 opsi yaitu 1. Meletakkan barang dari inventory dan");
-        System.out.println("2. Memindahkan barang yang sudah ada di ruangan. ");
-        System.out.println("Jika kamu memilih untuk meletakkan barang dari inventory menu akan berpindah ke menu iventory yang");
-        System.out.println("akan menampilkan keseluruhan inventory milikmu");
-        System.out.println("Kemudian silakan pilih barang apa yang akam kamu taruh di dalam ruangan");
-        System.out.println("Jika kamu memilih memindahkan barang yang sudah ada di ruangan, maka kamu akan diminta untuk memasukkan");
-        System.out.println("koordinat lokasi penempatan barang yang baru");
-        System.out.println("Add Sim akan meminta kamu untuk memasukkan nama SIm yang baru untuk dapat dimainkan");
-        System.out.println("Change Sim akan meminta kamu untuk memasukkan nama Sim yang sudah dibuat sebelumnya untuk dapat diganti");
-        System.out.println("List Of Object akan menampilkan barang apa saja yang ada di dalam ruangan");
-        System.out.println("Go To Object akan meminta kamu memilih untuk menuju ke barang apa yang berada di ruangan untuk dapat dilakukan aksi,");
-        System.out.println("jangan lupa barang harus ada di ruangan terlebih dahulu agar dapat didekati!");
-        System.out.println("Action akan meminta kamu untuk memilih untuk melakukan aksi apa terhadap object yang ada di dekatmu");
-        System.out.println("Buy Item akan meminta kamu untuk memilih barang apa yang ingin kamu beli, pastikan kamu ada uang yang cukup untuk membelinya!");
-        System.out.println("Selamat bermain!!");
+        System.out.printf("\n"+"===============================\t\tCOMMAND GUIDE\t\t==============================="+"\n\n");
+        System.out.println("1.  Help akan meminta kamu memilih 2 pilihan antara How To Play dan Command Guide");
+        System.out.println("2.  Exit akan mengakhiri permainan");
+        System.out.println("3.  View Sim Info akan menampilkan profil dan stats dari Sim yang sedang dimainkan");
+        System.out.println("4.  View Current Location akan menampilkan lokasi persis dari Sim yang sedang dimainkan");
+        System.out.println("5.  View Inventory akan menampilkan barang apa yang dimiliki Sim yang sedang dimainkan ");
+        System.out.println("6.  Upgrade House akan meminta kamu untuk memasukkan ruangan apa yang akan ditambahkan ruangan tetangga,");
+        System.out.println("    sehingga dapat dikatakan bahwa menu Upgrade House berarti menambah ruangan dalam rumah");
+        System.out.println("7.  Move Room akan memindahkan ruangan yang saling terhubung agar dapat dpindahkan");
+        System.out.println("8.  Edit Room akan meminta kamu untuk memilih 2 opsi yaitu 1. Meletakkan barang dari inventory dan");
+        System.out.println("    2. Memindahkan barang yang sudah ada di ruangan. ");
+        System.out.println(" -  Jika kamu memilih untuk meletakkan barang dari inventory menu akan berpindah ke menu iventory yang");
+        System.out.println("    akan menampilkan keseluruhan inventory milikmu");
+        System.out.println("    Kemudian silakan pilih barang apa yang akam kamu taruh di dalam ruangan");
+        System.out.println(" -  Jika kamu memilih memindahkan barang yang sudah ada di ruangan, maka kamu akan diminta untuk memasukkan");
+        System.out.println("    koordinat lokasi penempatan barang yang baru");
+        System.out.println("9.  Add Sim akan meminta kamu untuk memasukkan nama SIm yang baru untuk dapat dimainkan");
+        System.out.println("10.  Change Sim akan meminta kamu untuk memasukkan nama Sim yang sudah dibuat sebelumnya untuk dapat diganti");
+        System.out.println("11. List Of Object akan menampilkan barang apa saja yang ada di dalam ruangan");
+        System.out.println("12. Go To Object akan meminta kamu memilih untuk menuju ke barang apa yang berada di ruangan untuk dapat dilakukan aksi,");
+        System.out.println("    jangan lupa barang harus ada di ruangan terlebih dahulu agar dapat didekati!");
+        System.out.println("13. Action akan meminta kamu untuk memilih untuk melakukan aksi apa terhadap object yang ada di dekatmu");
+        System.out.println("14. Buy Item akan meminta kamu untuk memilih barang apa yang ingin kamu beli, pastikan kamu ada uang yang cukup yaa!");
+        System.out.println("***Selamat bermain!!***");
     }
  }
--- src/SingleHTP.java
+++ src/SingleHTP.java
@@ -13,14 +13,15 @@ public class SingleHTP {
     }
  
     public void showMessage(){
-       System.out.println("Pada awal permainan pilih salah satu diantara 3 command, yaitu Start, Help, dan Exit");
-       System.out.println("Apabila kamu memilih Help, maka menu akan menampilkan menu Help yang berisi penjelasan dari command");
-       System.out.println("Jika kamu memilih Exit, maka permainan selesai");
-       System.out.println("Jika kamu memilih Start, maka permainan akan dimulai dengan");
-       System.out.println("konfirmasi apakah kamu mau melakukan load file yang sudah pernah disimpan sebelumnya");
-       System.out.println("Jika iya silakan ketik (y), lalu tuliskan nama file yang ingin kamu load");
-       System.out.println("Jika tidak silakan klik (n) dan kamu akan memulai permainan dari awal dengan mode new game");
-       System.out.println("Setelah kamu masuk ke dalam permainan dari mode new game, silakan ketikkan nama Sim kamu");
-       System.out.println("Game akan berpindah menu menjadi menu game yang berisi 14 command yang dapat kamu gunakan selama permainan");
+      System.out.printf("\n"+"===============================\t\tHOW TO PLAY\t\t==============================="+"\n\n");
+       System.out.println("1.  Pada awal permainan pilih salah satu diantara 3 command, yaitu Start, Help, dan Exit");
+       System.out.println("2.  Apabila kamu memilih Help, penjelasannya berada pada Command Guide");
+       System.out.println("3.  Apabila kamu memilih Exit, penjelasannya berada pada Command Guide");
+       System.out.println("4.  Apabila kamu memilih Start, maka permainan akan dimulai dengan");
+       System.out.println("    konfirmasi apakah kamu mau melakukan load file yang sudah pernah disimpan sebelumnya");
+       System.out.println(" -  Apabila iya silakan ketik (y), lalu tuliskan nama file yang ingin kamu load");
+       System.out.println(" -  Apabila tidak silakan klik (n) dan kamu akan memulai permainan dari awal dengan mode new game");
+       System.out.println("5.  Setelah kamu masuk ke dalam permainan dari mode new game, silakan ketikkan nama Sim kamu");
+       System.out.println("6.  Game akan berpindah menu menjadi menu game yang berisi 14 command yang dapat kamu gunakan selama permainan");
     }
  }
